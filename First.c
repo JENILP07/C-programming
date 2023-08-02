373d97fca83bcd286885eb673ec5006ba72fe732
JENILP07 <140873633+JENILP07@users.noreply.github.com>
2023-08-02 09:14:57 +0530

    Add files via upload

---
+++ First.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+ int main(){
+ 	printf("Name     =\t\"Jenil Patel\"\n");
+    printf("\fSemester =\t\"First \"\n");
+    printf("\fRoll No  =\t\"154\"\n");
+    printf("\fAddress  =\t\"Shiv Park 1 BLock 21 Nirmala Road\v\t\t RAjkot-360007\"\n");
+    return 0;
+ } 
