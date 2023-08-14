7a169f7ffec8854b03fbfe59818cbc54a6fa5ad3
JENILP07 <140873633+JENILP07@users.noreply.github.com>
2023-08-14 18:27:41 +0530

    Avrage of 3 numbers

---
+++ x3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    int a,b,c,d;
+printf("Enter Val:");
+scanf("%d",&a);
+printf("Enter Val:");
+scanf("%d",&b);
+printf("Enter Val:");
+scanf("%d",&c);
+d=(a+b+c)/3;
+printf("Avg=%d",d);
+}
